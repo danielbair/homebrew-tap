1efc1f900dfe9a5e5831db7350a52d6a1dc5251d
Daniel Bair <bair.daniel@gmail.com>
2016-07-01 06:56:30 +0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# homebrew-tap
+## Installing it
+
+libespeak is available from my [formulae tap](https://github.com/danielbair/homebrew-tap). Add the tap:
+
+`brew tap danielbair/tap`
+
+Then install as any other formula:
+
+`brew install libespeak`
---
+++ libespeak.rb
@@ -0,0 +1,22 @@
+class Libespeak < Formula
+  desc "Static library for the eSpeak text to speech, software speech synthesizer"
+  homepage "http://espeak.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/espeak/espeak/espeak-1.48/espeak-1.48.04-source.zip"
+
+  depends_on "portaudio"
+
+  patch do
+    url "https://github.com/danielbair/homebrew-tap/raw/master/libespeak/Makefile.patch"
+    url "https://github.com/danielbair/homebrew-tap/raw/master/libespeak/fifo.cpp.patch"
+    url "https://github.com/danielbair/homebrew-tap/raw/master/libespeak/event.cpp.patch"
+  end
+
+  def install
+    share.install "espeak-data"
+    cd "src" do
+      rm "portaudio.h"
+      system "make", "libespeak.a", "DATADIR=#{share}/espeak-data", "PREFIX=#{prefix}"
+    end
+  end
+
+end
