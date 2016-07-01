16a92c112aa48110cb1cd4d4bbd4e9b1caafcb3f
Daniel Bair <bair.daniel@gmail.com>
2016-07-01 07:05:17 +0700

    Undo patches fix

--- libespeak.rb
+++ libespeak.rb
@@ -6,11 +6,11 @@ class Libespeak < Formula
   depends_on "portaudio"
 
   patch do
-    cd "src" do
+    #cd "src" do
       url "https://github.com/danielbair/homebrew-tap/raw/master/libespeak/Makefile.patch"
       url "https://github.com/danielbair/homebrew-tap/raw/master/libespeak/fifo.cpp.patch"
       url "https://github.com/danielbair/homebrew-tap/raw/master/libespeak/event.cpp.patch"
-    end
+    #end
   end
 
   def install
