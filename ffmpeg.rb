151ded81a05c92c9cbd57d06665fceeb89b19160
Daniel Bair <bair.daniel@gmail.com>
2016-07-04 22:27:14 +0700

    Update ffmpeg.rb sha256

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -2,6 +2,7 @@ class Ffmpeg < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-3.0.2.tar.bz2"
+  sha256 "30e3c77c2f4c358ed087869455a7496cbd7753a5e1b98d20ba49c1004009fd36"
 
   option "without-x264", "Disable H.264 encoder"
   option "without-lame", "Disable MP3 encoder"
