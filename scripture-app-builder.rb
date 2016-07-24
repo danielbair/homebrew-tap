128abcfd865b537bd2696ffb1bfcbf9a79c5d36c
Daniel Bair <bair.daniel@gmail.com>
2016-07-24 11:03:00 +0700

    Update scripture-app-builder.rb source url

--- scripture-app-builder.rb
+++ scripture-app-builder.rb
@@ -2,9 +2,8 @@ cask 'scripture-app-builder' do
   version '1.11.1'
   sha256 'd2abb7c221c99796f20b4b739c69eae6090987bb5fff80b0ed5877b0cc08c567'
 
-  # url 'http://software.sil.org/downloads/scriptureappbuilder/ScriptureAppBuilder-1.11.1.dmg'
-  # danielbair.com was verified as official when first introduced to the cask
-  url 'http://www.danielbair.com/ww2/ScriptureAppBuilder-1.11.1.dmg'
+  # software was verified as official when first introduced to the cask
+  url 'http://software.sil.org/downloads/scriptureappbuilder/ScriptureAppBuilder-1.11.1.dmg'
   name 'Scripture App Builder'
   homepage 'http://software.sil.org/scriptureappbuilder/'
   license :apache
