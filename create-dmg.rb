44864f6492a848fe6ab60db77b4a78175581b917
Daniel Bair <bair.daniel@gmail.com>
2016-07-24 11:00:59 +0700

    Update create-dmg.rb source url

---
+++ create-dmg.rb
@@ -0,0 +1,22 @@
+class CreateDmg < Formula
+  desc "ishell script to build fancy DMGs."
+  homepage "https://github.com/danielbair/create-dmg"
+  url "https://github.com/danielbair/create-dmg.git"
+  version "1.0.0.4"
+
+  def install
+    cp_r ".", prefix
+    create_dmg_wrapper = <<-EOS.undent
+    #!/bin/bash
+    # A wrapper for create-dmg to call it with it's support folders and files
+    IFS=$'\n'
+    #{prefix}/create-dmg "$@"
+    EOS
+    (buildpath/"create-dmg-wrapper").write create_dmg_wrapper
+    bin.install "create-dmg-wrapper" => "create-dmg"
+  end
+
+  test do
+    # printf result
+  end
+end
