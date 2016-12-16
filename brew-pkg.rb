a8ab013e53b885bc83e36c83078d748d7a84afe9
Daniel Bair <bair.daniel@gmail.com>
2016-12-16 08:13:53 -0500

    Updated brew-pkg.rb for new release and added head

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -1,9 +1,10 @@
 class BrewPkg < Formula
   desc "Homebrew command for building OS X packages from installed formulae."
   homepage "https://github.com/danielbair/brew-pkg"
+  head "https://github.com/danielbair/brew-pkg.git"
   url "https://github.com/danielbair/brew-pkg.git",
-      :tag => "v0.1.8",
-      :revision => "9c7b5e17379f8c2c6c0b92e270dc4316500b0458"
+      :tag => "v0.1.9",
+      :revision => "75f93682a92da89932ee7036875c3e5b07e95da2"
 
   # This is an .rb that must be executable in order for Homebrew to
   # find it with the 'which' method, so we skip_clean
