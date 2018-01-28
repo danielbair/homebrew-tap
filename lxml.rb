4d1acf44b0de0def66addbe9a759b0ed38ea3a0c
Daniel Bair <daniel@danielbair.com>
2018-01-28 19:10:53 +0700

    Update lxml.rb

--- lxml.rb
+++ lxml.rb
@@ -4,8 +4,8 @@ class Lxml < Formula
   url "https://pypi.python.org/packages/11/1b/fe6904151b37a0d6da6e60c13583945f8ce3eae8ebd0ec763ce546358947/lxml-3.6.0.tar.gz"
   sha256 "9c74ca28a7f0c30dca8872281b3c47705e21217c8bc63912d95c9e2a7cac6bdf"
 
-  depends_on :python => :recommended
-  depends_on :python3 => :optional
+  depends_on "python" => :recommended
+  depends_on "python3" => :optional
 
   def install
     Language::Python.each_python(build) do |python, version|
