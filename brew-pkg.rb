c628d8a1137afcc679c4deff2b28d0b83d51392d
Daniel Bair <bair.daniel@gmail.com>
2019-08-15 09:43:59 +0700

    Update brew-pkg.rb
    Added sha256 to brew-pkg formula

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -3,6 +3,7 @@ class BrewPkg < Formula
   homepage "https://github.com/danielbair/brew-pkg"
   head "https://github.com/danielbair/brew-pkg.git"
   url "https://github.com/danielbair/brew-pkg/archive/v0.1.11.tar.gz"
+  sha256 "d9fa73dc95e392cee91a41e662af90154fcf86db4b395cc59763bfd642809b49"
 
   # This is an .rb that must be executable in order for Homebrew to
   # find it with the 'which' method, so we skip_clean
