a54a6bded12812f63a18b58afbc99b8b1207c22e
Daniel Bair <bair.daniel@gmail.com>
2019-08-22 17:04:41 +0700

    Update espeak.rb

--- espeak.rb
+++ espeak.rb
@@ -1,9 +1,9 @@
 class Espeak < Formula
   desc "Text to speech, software speech synthesizer"
-  homepage "http://espeak.sourceforge.net/"
+  homepage "https://espeak.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/espeak/espeak/espeak-1.48/espeak-1.48.04-source.zip"
   sha256 "bf9a17673adffcc28ff7ea18764f06136547e97bbd9edf2ec612f09b207f0659"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://github.com/danielbair/homebrew-tap/releases/download/bottles/"
@@ -25,13 +25,15 @@ class Espeak < Formula
       inreplace "speech.h", "#define USE_ASYNC", "//#define USE_ASYNC"
       # OS X does not provide sem_timedwait() so disabling #define USE_ASYNC to compile for OS X.
       # See https://sourceforge.net/p/espeak/discussion/538922/thread/0d957467/#407d
+      inreplace "synthdata.cpp", "1.48.03  04.Mar.14", "1.48.04  06.04.2014"
       system "make", "speak", "DATADIR=#{espeak_data}", "PREFIX=#{prefix}"
       bin.install "speak" => "espeak"
       system "make", "libespeak.a", "DATADIR=#{espeak_data}", "PREFIX=#{prefix}"
       lib.install "libespeak.a" => "libespeak.a"
       system "make", "libespeak.so", "DATADIR=#{espeak_data}", "PREFIX=#{prefix}"
       lib.install "libespeak.so.1.1.48" => "libespeak.dylib"
-      system "install_name_tool", "-id", "#{lib}/libespeak.dylib", "#{lib}/libespeak.dylib"
+      MachO::Tools.change_dylib_id("#{lib}/libespeak.dylib", "#{lib}/libespeak.dylib")
+      #system "install_name_tool", "-id", "#{lib}/libespeak.dylib", "#{lib}/libespeak.dylib"
       # OS X does not use the convention libraryname.so.X.Y.Z. OS X uses the convention libraryname.X.dylib
       # See http://stackoverflow.com/questions/4580789/ld-unknown-option-soname-on-os-x/32280483#32280483
     end
