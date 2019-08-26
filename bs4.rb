845cf519f4525ba62d25281c813b7bb432fcfd96
Daniel Bair <bair.daniel@gmail.com>
2019-08-26 13:38:36 +0700

    Update bs4.rb
    Integrate soupsieve

--- bs4.rb
+++ bs4.rb
@@ -11,14 +11,23 @@ class Bs4 < Formula
   #  sha256 "e61398a853973e937330cee86e48500389bd5fce534980f52a1c18835e9727a9" => :high_sierra
   #end
 
-  depends_on "soupsieve"
-  depends_on "python" => :recommended
+  depends_on "python"
   depends_on "python2" => :optional
+
+  #depends_on "soupsieve"
+  
+  resource "soupsieve" do
+    url "https://files.pythonhosted.org/packages/6b/77/b7801323fd321021d92efb11154b7b85410318b8a2e9757c410afb6d976f/soupsieve-1.9.3.tar.gz"
+    #sha256 "3c9474036afda9136aac6463def733f81017bf9ef3510d25634f335b0c87f5e1"
+  end
 
   def install
     Language::Python.each_python(build) do |python, version|
       dest_path = lib/"python#{version}/site-packages"
       dest_path.mkpath
+      resource("soupsieve").stage do
+        system python, *Language::Python.setup_install_args(prefix)
+      end
       system python, *Language::Python.setup_install_args(prefix)
     end
   end
