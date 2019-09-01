60bd8f82410d84ae54d3bc286c3a0149bce97477
Daniel Bair <bair.daniel@gmail.com>
2019-09-01 16:03:48 +0700

    Update deps
    Updated deps

--- backports.rb
+++ backports.rb
@@ -1,22 +1,20 @@
-class Soupsieve < Formula
-  desc "provides selectors from the CSS level 1 specifications up through the latest CSS level 4 drafts and beyond"
-  homepage "https://github.com/facelessuser/soupsieve"
-  url "https://files.pythonhosted.org/packages/6b/77/b7801323fd321021d92efb11154b7b85410318b8a2e9757c410afb6d976f/soupsieve-1.9.3.tar.gz"
-  sha256 "8662843366b8d8779dec4e2f921bebec9afd856a5ff2e82cd419acc5054a1a92"
+class Backports < Formula
+  desc "Backport of functools.lru_cache from Python 3.3 as published at ActiveState."
+  homepage "https://github.com/jaraco/backports.functools_lru_cache"
+  url "https://files.pythonhosted.org/packages/57/d4/156eb5fbb08d2e85ab0a632e2bebdad355798dece07d4752f66a8d02d1ea/backports.functools_lru_cache-1.5.tar.gz"
+  sha256 "9d98697f088eb1b0fa451391f91afb5e3ebde16bbdb272819fd091151fda4f1a"
 
-  bottle do
-    root_url "https://github.com/danielbair/homebrew-tap/releases/download/bottles/"
-    cellar :any_skip_relocation
-    sha256 "74269451608ed19de14928c5ee8b973730355a717d60523833ca62b8710c7a43" => :yosemite
-  end
+  #bottle do
+  #  root_url "https://github.com/danielbair/homebrew-tap/releases/download/bottles/"
+  #  cellar :any_skip_relocation
+  #  sha256 "74269451608ed19de14928c5ee8b973730355a717d60523833ca62b8710c7a43" => :yosemite
+  #end
 
   depends_on "python"
   depends_on "python@2"
 
-  depends_on "backports"
-
   def install
-    ["python2", "python3"].each do |python|
+    ["python", "python2"].each do |python|
       version = Language::Python.major_minor_version python
       dest_path = lib/"python#{version}/site-packages"
       dest_path.mkpath
