d87db08c1522042ae11e83ec72ed8155f17a49d7
Daniel Bair <bair.daniel@gmail.com>
2019-09-07 19:37:26 +0700

    Update ffmpeg.rb

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -99,9 +99,9 @@ class Ffmpeg < Formula
     chmod 0755, bin/"python/convert_from_tensorflow.py"
     file_prepend(bin/"python/convert.py", "#!/usr/bin/env python")
     file_prepend(bin/"python/convert_from_tensorflow.py", "#!/usr/bin/env python")
-    mv bin/"python/convert_from_tensorflow.py", bin/
-    mv bin/"python/convert.py", bin/
-    rmdir bin/"python"
+    mv(bin/"python/convert_from_tensorflow.py", bin/)
+    mv(bin/"python/convert.py", bin/)
+    rmdir(bin/"python")
   end
 
   test do
