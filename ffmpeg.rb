220f467c3b02d2686cf386dea78af60fe879a6d7
Daniel Bair <bair.daniel@gmail.com>
2019-09-07 20:43:30 +0700

    Update ffmpeg.rb

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -3,7 +3,7 @@ class Ffmpeg < Formula
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-4.2.1.tar.xz"
   sha256 "cec7c87e9b60d174509e263ac4011b522385fd0775292e1670ecc1180c9bb6d4"
-  # revision 1
+  revision 5
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   bottle do
@@ -97,8 +97,8 @@ class Ffmpeg < Formula
     chmod 0755, bin/"python"
     chmod 0755, bin/"python/convert.py"
     chmod 0755, bin/"python/convert_from_tensorflow.py"
-    file_prepend(bin/"python/convert.py", "#!/usr/bin/env python")
-    file_prepend(bin/"python/convert_from_tensorflow.py", "#!/usr/bin/env python")
+    file_prepend(bin/"python/convert.py", "#!/usr/bin/env python\n")
+    file_prepend(bin/"python/convert_from_tensorflow.py", "#!/usr/bin/env python\n")
     mv bin/"python/convert_from_tensorflow.py", bin/"convert_from_tensorflow.py", :force => true
     mv bin/"python/convert.py", bin/"convert.py", :force => true
     rmdir bin/"python"
