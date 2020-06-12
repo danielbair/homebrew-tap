c2d0b715777bd0aad45e6e38421efb1e8a40c9a2
Daniel Bair <bair.daniel@gmail.com>
2020-06-12 15:31:18 +0700

    Update aeneas.rb
    Homebrew dropped python@2

--- aeneas.rb
+++ aeneas.rb
@@ -4,10 +4,6 @@ class Aeneas < Formula
   url "https://github.com/readbeyond/aeneas/archive/v1.7.3.tar.gz"
   sha256 "cd6453526a7a274df113d353a45ee270b6e912f91fc8b346e2d12847b5219f61"
   head "https://github.com/readbeyond/aeneas.git", :branch => "master"
-  devel do
-    url "https://github.com/readbeyond/aeneas.git", :branch => "devel"
-    version "1.7.4"
-  end
 
   bottle do
     root_url "https://github.com/danielbair/homebrew-tap/releases/download/bottles"
@@ -15,14 +11,12 @@ class Aeneas < Formula
     sha256 "9e85e8aaf451bf32ea4a8f4fbab52f3165e16e20b3d82dc5706077dbdb5fce56" => :yosemite
   end
 
-  depends_on "danielbair/tap/espeak"
-  depends_on "ffmpeg"
-  depends_on "python"
-  depends_on "python@2"
-
-  depends_on "numpy"
-  depends_on "danielbair/tap/lxml"
   depends_on "danielbair/tap/bs4"
+  depends_on "danielbair/tap/espeak"
+  depends_on "danielbair/tap/lxml"
+  depends_on "ffmpeg"
+  depends_on "numpy"
+  depends_on "python"
 
   patch do
     url "https://github.com/readbeyond/aeneas/pull/204.patch?full_index=1"
@@ -30,7 +24,7 @@ class Aeneas < Formula
   end
 
   def install
-    ["python2", "python3"].each do |python|
+    ["python", "python3"].each do |python|
       version = Language::Python.major_minor_version python
       dest_path = lib/"python#{version}/site-packages/aeneas"
       dest_path.mkpath
@@ -41,15 +35,15 @@ class Aeneas < Formula
   end
 
   def caveats
-      homebrew_site_packages = Language::Python.homebrew_site_packages "python2.7"
-      user_site_packages = Language::Python.user_site_packages "python"
-      <<~EOS
-        If you use system python (that comes - depending on the OS X version -
-        with older versions of numpy, scipy and matplotlib), you may need to
-        ensure that the brewed packages come earlier in Python's sys.path with:
-          mkdir -p #{user_site_packages}
-          echo 'import sys; sys.path.insert(1, "#{homebrew_site_packages}")' >> #{user_site_packages}/homebrew.pth
-      EOS
+    homebrew_site_packages = Language::Python.homebrew_site_packages "python"
+    user_site_packages = Language::Python.user_site_packages "python"
+    <<~EOS
+      If you use system python (that comes - depending on the OS X version -
+      with older versions of numpy, scipy and matplotlib), you may need to
+      ensure that the brewed packages come earlier in Python's sys.path with:
+        mkdir -p #{user_site_packages}
+        echo 'import sys; sys.path.insert(1, "#{homebrew_site_packages}")' >> #{user_site_packages}/homebrew.pth
+    EOS
   end
 
   test do
