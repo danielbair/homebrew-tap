ace93bb0a7dbc897e40915e32927bbf961a13687
Daniel Bair <bair.daniel@gmail.com>
2020-06-15 11:00:00 +0700

    Update aeneas.rb
    Detect utf-8 shell env in python3.8

--- aeneas.rb
+++ aeneas.rb
@@ -3,19 +3,19 @@ class Aeneas < Formula
   homepage "http://www.readbeyond.it/aeneas/"
   url "https://github.com/readbeyond/aeneas/archive/v1.7.3.tar.gz"
   sha256 "cd6453526a7a274df113d353a45ee270b6e912f91fc8b346e2d12847b5219f61"
-  revision 2
+  revision 3
   head "https://github.com/readbeyond/aeneas.git", :branch => "master"
 
   bottle do
     root_url "https://github.com/danielbair/homebrew-tap/releases/download/bottles"
     cellar :any
-    sha256 "9b2704c8262f771d385ceee7a6d3363cd18ff5fd5874c0f5ad27550b9e0b2f75" => :yosemite
+    sha256 "c399a48a9ab9040780163d8c53ef835bd9a0dcf6f5e6308547c0307c77ac6093" => :yosemite
   end
 
   depends_on "danielbair/tap/bs4"
   depends_on "danielbair/tap/espeak"
   depends_on "danielbair/tap/lxml"
-  depends_on "ffmpeg"
+#  depends_on "ffmpeg"
   depends_on "numpy"
   depends_on "python@3.8"
 
@@ -24,8 +24,8 @@ class Aeneas < Formula
     sha256 "ab25dd5f519bfca1fb5f9a865d5654178d14acf3f67e6b758e657de8d8521f82"
   end
   patch do
-    url "https://github.com/danielbair/aeneas/commit/0d06d884b1c886c9912c2921c1c1b5fbf6024299.patch"
-    sha256 "4d47a7fa7a92df9acce78879a8a12a496fa152e8f960e71584ab8adede82c9dc"
+    url "https://github.com/readbeyond/aeneas/pull/255.patch?full_index=1"
+    sha256 "1ae53d539f4f8118f9b8be99549fe8b4aeb19bcb2488894b30fac15abd12154b"
   end
 
   def install
@@ -40,7 +40,7 @@ class Aeneas < Formula
   end
 
   test do
-    result = `export PYTHONIOENCODING=UTF-8; export PYTHONPATH=#{ENV["PYTHONPATH"]}:$PYTHONPATH; /usr/local/opt/python\@3.8/bin/python3 -m aeneas.diagnostics; /usr/local/opt/python\@3.8/bin/python3 -m aeneas.tools.synthesize_text list "This is a test|with two lines" eng -v test.wav`
+    result = `export PYTHONIOENCODING=UTF-8; export PYTHONPATH=#{ENV["PYTHONPATH"]}:$PYTHONPATH; /usr/local/opt/python\@3.8/bin/python3 -m aeneas.diagnostics; /usr/local/opt/python\@3.8/bin/python3 -m aeneas.tools.synthesize_text list "This is a test|with two lines" eng -v test.wav; rm -f test.wav`
     printf result
   end
 end
