b3454d7c38361b69d1777f3ad1da79b62224d3ab
Daniel Bair <bair.daniel@gmail.com>
2020-06-15 20:04:56 +0700

    Update brew-pkg.rb

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -2,8 +2,8 @@ class BrewPkg < Formula
   desc "Homebrew command for building OS X packages from installed formulae."
   homepage "https://github.com/danielbair/brew-pkg"
   head "https://github.com/danielbair/brew-pkg.git"
-  url "https://github.com/danielbair/brew-pkg/archive/v0.1.11.tar.gz"
-  sha256 "d9fa73dc95e392cee91a41e662af90154fcf86db4b395cc59763bfd642809b49"
+  url "https://github.com/danielbair/brew-pkg/archive/v0.1.12.tar.gz"
+  sha256 "91aa17b242b19f264ec22d4c8d4f79752075146603b8d4c5588d257e5d816557"
 
   # This is an .rb that must be executable in order for Homebrew to
   # find it with the 'which' method, so we skip_clean
