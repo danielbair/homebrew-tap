e2c4ec7206e7131834d0de76278cd2f1d9929470
Daniel Bair <bair.daniel@gmail.com>
2020-07-13 16:03:35 +0700

    Update espeak-ng.rb

--- espeak-ng.rb
+++ espeak-ng.rb
@@ -5,26 +5,23 @@ class EspeakNg < Formula
   sha256 "5ce9f24ee662b5822a4acc45bed31425e70d7c50707b96b6c1603a335c7759fa"
   head "https://github.com/danielbair/espeak-ng.git"
 
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "pcaudiolib" => :build
-  depends_on "sonic-speech" => :build
   depends_on "ronn-ng" => :build
   depends_on "kramdown" => :build
+  depends_on "pcaudiolib"
+  depends_on "sonic-speech"
 
   def install
     system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}", "--with-extdict-ru", "--with-extdict-zh", "--with-extdict-zhy"
-    system "make", "src/espeak-ng", "src/speak-ng"
-    system "make", "en"
-    system "make", "-i", "-k", "docs"
+    system "make", "src/espeak-ng", "src/speak-ng", "en"
     system "make", "-i", "-k", "install"
   end
 
   test do
-    #nothing
+    system "espeak-ng", "Testing.", "-w", "test.wav"
   end
 end
