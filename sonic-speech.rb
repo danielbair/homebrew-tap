d9be3e67535d22b7f1a12905fa0bb6cd822e5a46
Daniel Bair <bair.daniel@gmail.com>
2020-07-16 17:47:47 +0700

    Update sonic-speech.rb

--- sonic-speech.rb
+++ sonic-speech.rb
@@ -3,15 +3,21 @@ class SonicSpeech < Formula
   homepage "https://github.com/espeak-ng/sonic"
   url "https://github.com/espeak-ng/sonic/archive/release-0.2.0.tar.gz"
   sha256 "c7827ce576838467590ffa1f935fbe1049e896dfed6c515cf569ad3779c24085"
+  revision 1
 
   bottle do
     root_url "https://github.com/danielbair/homebrew-tap/releases/download/bottles"
     cellar :any
-    sha256 "4ae710f9c821f6144180960d030dae3e83f001b5d207c54f57d8db86007127af" => :yosemite
+    #sha256 "4ae710f9c821f6144180960d030dae3e83f001b5d207c54f57d8db86007127af" => :yosemite
   end
 
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
+
+  patch do
+    url "https://raw.githubusercontent.com/danielbair/homebrew-tap/7a24591f07c07e87f490823f620f6f4175aefbb9/patches/patch-sonic-mac.diff"
+    sha256 "d84f9e5be4c81fc7a5ebcc672d02b171b59ca4c17512d823b2deb4fe0e9ceed0"
+  end
 
   def install
     system "make", "USE_SPECTROGRAM=0", "PREFIX=#{prefix}"
